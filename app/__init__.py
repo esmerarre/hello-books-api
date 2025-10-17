9aa69ef85039a0cc43c5ae99ab0a1f0a194edd32
Esmeralda Arreguin-Martinez <esmerarre@gmail.com>
2025-10-16 18:47:03 -0700

    read all books  complete

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
-from .routes.hello_world_routes import hello_world_bp
+from .routes.book_routes import books_bp
+#from .routes.hello_world_routes import hello_world_bp #Not needed for hello books API
 
 def create_app():
     app = Flask(__name__)
 
     # Register Blueprints here
-    app.register_blueprint(hello_world_bp)
+    app.register_blueprint(books_bp)
+    # This code not needed for hello books API
+    #app.register_blueprint(hello_world_bp)
     
     return app
