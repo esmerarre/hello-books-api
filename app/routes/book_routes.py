8acd9c0e4888a49a520b933e2ab014f6a9b63298
Esmeralda Arreguin-Martinez <esmerarre@gmail.com>
2025-11-02 11:09:32 -0800

    added to_dict and associated tests

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -56,11 +56,7 @@ def create_book():
     db.session.add(new_book)
     db.session.commit()
 
-    response = {
-        "id": new_book.id,
-        "title": new_book.title,
-        "description": new_book.description,
-    }
+    response = new_book.to_dict()
     return response, 201
 
 @books_bp.get("")
@@ -83,26 +79,17 @@ def get_all_books():
 
     books_response = []
     for book in books:
-        books_response.append(
-            {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description
-            }
-        )
+        books_response.append(book.to_dict())
     return books_response
 
 @books_bp.get("/<book_id>")
 def get_one_book(book_id):
     book = validate_book(book_id)
+
     # We could also write the line above as:
     # books = db.session.execute(query).scalars()
 
-    return {
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-            }
+    return book.to_dict()
 
 
 @books_bp.put("/<book_id>")
@@ -128,14 +115,14 @@ def validate_book(book_id):
     try:
         book_id = int(book_id)
     except:
-        response = {"message": f"book {book_id} invalid"}
+        response = {"message": f"Book {book_id} invalid"}
         abort(make_response(response , 400))
 
     query = db.select(Book).where(Book.id == book_id)
     book = db.session.scalar(query)
     
     if not book:
-        response = {"message": f"book {book_id} not found"}
+        response = {"message": f"Book {book_id} not found"}
         abort(make_response(response, 404))
 
     return book
