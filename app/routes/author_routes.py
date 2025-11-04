d3154191533e1d09b8a805ef3fe71948677d84fb
Esmeralda Arreguin-Martinez <esmerarre@gmail.com>
2025-11-03 18:50:22 -0800

    refactored routes

--- app/routes/author_routes.py
+++ app/routes/author_routes.py
@@ -1,42 +1,19 @@
 from flask import Blueprint, abort, make_response, request, Response
 from ..db import db
 from app.models.author import Author
-from .route_utilities import validate_model
+from app.models.book import Book
+from .route_utilities import validate_model, create_model, get_models_with_filters
 
 bp = Blueprint("author_bp", __name__, url_prefix="/authors")
 
 @bp.post("")
 def create_author():
     request_body = request.get_json()
-    try:
-        new_author = Author.from_dict(request_body)
-
-    except KeyError as error:
-        response = {"message": f"Invalid request: missing {error.args[0]}"}
-        abort(make_response(response, 400))
-
-    db.session.add(new_author)
-    db.session.commit()
-
-    response = new_author.to_dict()
-    return response, 201
+    return create_model(Author, request_body)
 
 @bp.get("")
 def get_all_author():
-    query = db.select(Author)
-
-    name_param = request.args.get("name")
-    if name_param:
-        query = query.where(Author.title.ilike(f"%{name_param}%"))
-
-    query = query.order_by(Author.id)
-
-    authors = db.session.scalars(query)
-    # We could also write the line above as:
-    # books = db.session.execute(query).scalars()
-    authors_response = [author.to_dict() for author in authors]
-
-    return authors_response
+    return get_models_with_filters(Author, request.args)
 
 @bp.post("/<author_id>/books")
 def create_book_with_author(author_id):
@@ -45,17 +22,7 @@ def create_book_with_author(author_id):
     request_body = request.get_json()
     request_body["author_id"] = author.id
 
-    try:
-        new_book = Book.from_dict(request_body)
-
-    except KeyError as error:
-        response = {"message": f"Invalid request: missing {error.args[0]}"}
-        abort(make_response(response, 400))
-        
-    db.session.add(new_book)
-    db.session.commit()
-
-    return make_response(new_book.to_dict(), 201)
+    return create_model(Book, request_body)
 
 @bp.get("/<author_id>/books")
 def get_books_by_author(author_id):
--- tests/test_route_utilities.py
+++ tests/test_route_utilities.py
@@ -1,6 +1,7 @@
 from werkzeug.exceptions import HTTPException
-from app.routes.route_utilities import validate_model
+from app.routes.route_utilities import validate_model, create_model, get_models_with_filters
 from app.models.book import Book
+from app.models.author import Author
 import pytest
 
 def test_validate_book(two_saved_books):
@@ -33,3 +34,64 @@ def test_validate_book_invalid_id(two_saved_books):
     response = error.value.response
     assert response.status == "400 BAD REQUEST"
 
+# We use the `client` fixture because we need an
+# application context to work with the database session
+def test_create_model_book(client):
+    # Arrange
+    test_data = {
+        "title": "New Book",
+        "description": "The Best!"
+    }
+
+    # Act
+    result = create_model(Book, test_data)
+
+    # Assert
+    assert isinstance(result, tuple)
+    assert result[0]["id"] == 1
+    assert result[0]["title"] == "New Book"
+    assert result[0]["description"] == "The Best!"
+    assert result[1] == 201
+
+
+def test_create_model_book_missing_data(client):
+    # Arrange
+    test_data = {
+        "description": "The Best!"
+    }
+
+    # Act & Assert
+    # Calling `create_model` without being invoked by a route will
+    # cause an `HTTPException` when an `abort` statement is reached 
+    with pytest.raises(HTTPException) as error:
+        result_book = create_model(Book, test_data)
+
+    response = error.value.response
+    assert response.status == "400 BAD REQUEST"
+
+def test_create_model_author(client):
+    # Arrange
+    test_data = {
+        "name": "New Author"
+    }
+
+    # Act
+    result = create_model(Author, test_data)
+
+    # Assert
+    assert isinstance(result, tuple)
+    assert result[0]["id"] == 1
+    assert result[0]["name"] == "New Author"
+    assert result[1] == 201
+
+
+def test_get_models_with_filters_one_matching_book(two_saved_books):
+    # Act
+    result = get_models_with_filters(Book, {"title": "ocean"})
+
+    # Assert
+    assert result == [{
+        "id": 1,
+        "title": "Ocean Book",
+        "description": "watr 4evr"
+    }]
