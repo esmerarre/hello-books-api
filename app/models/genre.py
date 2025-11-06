f6350729c312acf85dfe93af505d2613f37a131b
Esmeralda Arreguin-Martinez <esmerarre@gmail.com>
2025-11-05 23:42:26 -0800

    added genre

---
+++ app/models/genre.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from ..db import db
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+#   from .book import Book
+
+class Genre(db.Model):
+    id: Mapped[int]= mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str]
+
+    def to_dict(self):
+        genre_as_dict = {
+            "id": self.id,
+            "name": self.name
+        }
+        return genre_as_dict
+    
+    @classmethod
+    def from_dict(cls, genre_data):
+        new_genre = cls(name=genre_data["name"])
+        return new_genre
