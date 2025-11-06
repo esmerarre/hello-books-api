b50e0efe6613ec913074c66def0b92e0429581d3
Esmeralda Arreguin-Martinez <esmerarre@gmail.com>
2025-11-06 00:05:46 -0800

    established book_genre routes

--- app/models/book.py
+++ app/models/book.py
@@ -19,17 +19,19 @@ class Book(db.Model):
     genres: Mapped[list["Genre"]] = relationship(secondary="book_genre", back_populates="books") 
 
 
-
-    # indented under the Book class definition
     @classmethod
     def from_dict(cls, book_data):
         #Use get() to fetch values that could be undefined to avoid raising an error
         author_id = book_data.get("author_id")
-        new_book = Book(title=book_data["title"],
+        genres = book_data.get("genres", [])
+
+        new_book = cls(title=book_data["title"],
                         description=book_data["description"],
-                        author_id=author_id)
+                        author_id=author_id,
+                        genres=genres
+                        )
         return new_book
-    
+
     def to_dict(self):
         book_as_dict = {}
         book_as_dict["id"] = self.id
@@ -38,6 +40,9 @@ class Book(db.Model):
 
         if self.author:
             book_as_dict["author"] = self.author.name
+        
+        if self.genres:
+            book_as_dict["genres"] = [genre.name for genre in self.genres]
 
         return book_as_dict
 
